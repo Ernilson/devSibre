62b119f0e2ab7822fd29177b82c06d19c5a1bed5
Ernilson <ernilson25@gmail.com>
2020-06-10 17:06:28 -0300

    teste

--- src/main/java/br/com/devsibre/Model/FormularioModel.java
+++ src/main/java/br/com/devsibre/Model/FormularioModel.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 public class FormularioModel {
 	
 	@Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id_c;
     @Column(length = 200, nullable = false)
     private String nome;    
